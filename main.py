a39ccd799a93feeac672768b8fa89efc0772709e
Will Nilges <wdn5796@rit.edu>
2023-12-13 01:14:15 -0500

    I can get 36/40 links

--- main.py
+++ main.py
@@ -7,7 +7,26 @@ from uisp_client import UISPClient
 def main():
     load_dotenv()
     uisp = UISPClient()
-    print(json.dumps(uisp.get_data_links(), indent=2))
+    for l in uisp.get_data_links():
+
+        # For now, this software is only interested in Point to Point links,
+        # created by airMax
+
+        devices = ("airFiber", "airMax", "wave", "wlan")
+        wireless_modes = ("ap-ptp", "sta-ptp", "ap-ptmp-airmax-ac", "ap-ptmp-airmax-mixed")
+        series = ("classic", "G60", "LTU")
+
+        # Filter results
+        if l["type"] != "wireless":
+            continue
+
+#        if l["from"]["device"]["identification"]["type"] not in devices and l["to"]["device"]["identification"]["type"] not in devices:
+#            continue
+
+        if l["from"]["device"]["overview"]["wirelessMode"] not in wireless_modes or l["to"]["device"]["overview"]["wirelessMode"] not in wireless_modes:
+            continue
+
+        print(l["ssid"])
 
 if __name__ == "__main__":
     main()
